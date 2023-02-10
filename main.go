a7a8fa5c22c66b0261f127460723c3de2b38534f
Alex <alexdsa@yandex.ru>
2023-02-10 14:42:48 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
+type PrintFunc func(string)
+
+func Print (s string) {
+	fmt.Println(s)
+}
+
+func PrintEx(fn PrintFunc) PrintFunc {
+	return func(s string) {
+		fmt.Println("start")
+		fn(s)
+		fmt.Println("finish")
+	}
+}
+
+
 func main() {
-	goEncoder()
+	Print("hello")
+	PrintEx(Print)("world")
 }
